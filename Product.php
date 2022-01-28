a62f0572ac8c1cb7f3a1b9c4e98ea52a72e6f386
Deki97 <dejan97@libero.it>
2022-01-28 16:08:23 +0100

    class Product done

--- Product.php
+++ Product.php
@@ -1,2 +1,19 @@
 <?php 
+
+class Product {
+    public $name;
+
+    public $price;
+
+    public $category;
+
+    public $delivery_time;
+
+    public function __construct($_name, $_price, $_category, $_delivery_time) {
+        $this->name = $_name;
+        $this->price = $_price;
+        $this->category = $_category;
+        $this->delivery_time = $_delivery_time;
+    }
+}
 ?>
