03d4e119cf2d56002b33fc9aa70f6cd03491ddde
Deki97 <dejan97@libero.it>
2022-01-28 16:41:42 +0100

    class Book done

--- Book.php
+++ Book.php
@@ -1,2 +1,18 @@
 <?php 
+
+require_once __DIR__ . '/Product.php';
+
+class Book extends Product {
+    public $number_of_pages;
+
+    public $genre;
+
+    public $author;
+
+    public function __construct($_name, $_price, $_category, $_genre, $_author) {
+        parent::__construct($_name, $_price, $_category);
+        $this->genre = $_genre;
+        $this->author = $_author;
+    }
+}
 ?>
